b49c39837bdad343e21100b529ce212fc551c47d
Sahil Shrichand Talreja <157671842+talrejasahil4994@users.noreply.github.com>
2025-09-04 20:58:45 +0530

    Delete frontend directory

--- frontend/src/store/useThemeStore.js
+++
@@ -1,9 +0,0 @@
-import { create } from "zustand";
-
-export const useThemeStore = create((set) => ({
-  theme: localStorage.getItem("preferred-theme") || "forest",
-  setTheme: (theme) => {
-    localStorage.setItem("preferred-theme", theme);
-    set({ theme });
-  },
-}));
